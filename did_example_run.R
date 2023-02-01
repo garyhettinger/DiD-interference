18f0487d940e9a9b06c0553075fcddcb4a4cbf88
Gary Hettinger <105126893+garyhettinger@users.noreply.github.com>
2023-02-01 12:21:08 -0500

    Update did_example_run.R

--- did_example_run.R
+++ did_example_run.R
@@ -13,7 +13,7 @@ sim_data = get_simulated_dataset(seed=1, ps_true=T, outcome_true=T, n=1000, nper
 # Run doubly robust method
 
 estims = get_estimates(data=sim_data$data, trt_col_name="city", trts=3, neighbors=2, 
-                       trt_ctls=1, neighbor_ctls=4, nboots=3, period_col="period",
+                       trt_ctls=1, neighbor_ctls=4, nboots=10, period_col="period",
                        method="dr", ps_vars=c("W1", "W2", "W3", "W4"), 
                        or_vars=c("W1", "W2", "W3", "W4"), ps_ia_terms=c(), 
                        or_ia_terms=c())
